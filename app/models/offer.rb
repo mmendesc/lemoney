6b4d7e67bb9a14a8151fe123d54e26cc5a88ceed
Matheus Fernandes <matheus.mendescf@gmail.com>
2020-03-25 18:38:45 -0300

    Add offer validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,8 +2,24 @@
 
 class Offer < ApplicationRecord
 
+  validates_presence_of :advertiser_name, :description, :url, :starts_at
+  validates_uniqueness_of :advertiser_name
+  validates :description, length: { maximum: 500 }
+  validate :valid_url
+
   enum status: {
     disabled: 1,
     enabled: 2
   }
+
+  def valid_url
+    errors.add(:url, 'Invalid url') unless valid_url?
+  end
+
+  def valid_url?
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
 end
