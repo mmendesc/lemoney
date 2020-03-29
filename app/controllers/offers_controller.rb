186a902d59a86e7c1ef1d7d657bff31952bb8295
Matheus Fernandes <matheus.mendescf@gmail.com>
2020-03-29 12:43:28 -0300

    Fix enable/disabled and offers orders

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -4,6 +4,6 @@ class OffersController < ApplicationController
   # before_action :set_offer, only: [:show, :edit, :update, :destroy]
 
   def index
-    @offers = Offer.enabled
+    @offers = Offer.enabled.order(premium: :desc)
   end
 end
